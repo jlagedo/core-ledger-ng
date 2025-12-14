e212ca5f51d5dc329e0849ff8dcbfd60e15f9e3a
João Amaro Lagedo <jlagedo@gmail.com>
2025-12-14 13:03:15 -0300

    remove home component and route
    - Delete HomeComponent and related files (component, template, styles, spec)
    - Remove /home route from app routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', loadComponent: () => import('./dashboard-component/dashboard-component').then(c => c.DashboardComponent) },
-    { path: 'home', loadComponent: () => import('./home/home.component').then(c => c.HomeComponent) },
     { path: 'todo', loadComponent: () => import('./todo/todo').then(c => c.TodoComponent) },
 ];
