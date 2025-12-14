ed8bff7506c563dbf102428974df5a6cddd95c09
João Amaro Lagedo <jlagedo@gmail.com>
2025-12-13 22:57:41 -0300

    add proxy configuration and integrate todo list display in home component
    - Add proxy.conf.json to route /api requests to localhost:5204
    - Configure proxy in angular.json for development environment
    - Inject TodoApiService in home component
    - Add Observable stream for todo list using getTodos()
    - Display todo descriptions in home template using async pipe and @for loop
    - Import CommonModule and AsyncPipe in home component
    - Update environment.ts apiUrl to use proxied /api path
    - Remove unused home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
+import { TodoApiService } from '../services/todo-api.service';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/todo.model';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [],
+    imports: [CommonModule, AsyncPipe],
     templateUrl: './home.html',
 })
-export class HomeComponent { }
+export class HomeComponent {
+    private todoApiService = inject(TodoApiService);
+
+    // The dollar sign ($) is a convention in Angular to indicate that a property is an Observable stream
+    todoList$: Observable<Todo[]> = this.todoApiService.getTodos();
+}
